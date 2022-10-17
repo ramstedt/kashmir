3e12a6a236192f151826cf2584fca355e2eb1a78
Deliciaes <71063351+Deliciaes@users.noreply.github.com>
2022-10-17 15:10:32 +0200

    menu components

---
+++ components/consumerView/menu/MenuWrapper.jsx
@@ -0,0 +1,12 @@
+import MenuNav from "./navbar/MenuNav";
+
+export default function ConsumerWrapper({ children }) {
+  return (
+    <>
+      <MenuNav />
+      <div className="max-w-lg m-auto">
+        <div className="mx-[6%]">{children}</div>
+      </div>
+    </>
+  );
+}
